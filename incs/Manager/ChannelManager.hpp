80ca2d3724d06ce4301d3e811da27af348297351
Loup Martineau <loumarti@z2r12p2.42lyon.fr>
2023-04-01 09:54:24 +0200

    channelManager wip

--- incs/Manager/ChannelManager.hpp
+++ incs/Manager/ChannelManager.hpp
@@ -6,14 +6,16 @@
 /*   By: loumarti <loumarti@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/01 08:10:11 by loumarti          #+#    #+#             */
-/*   Updated: 2023/04/01 09:20:18 by loumarti         ###   ########lyon.fr   */
+/*   Updated: 2023/04/01 09:43:38 by loumarti         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CHANNELMANAGER_HPP
 # define CHANNELMANAGER_HPP
 
-# include "../Object/Server.hpp"
+# include "../Object/Channel.hpp"
+# include "../Object/Client.hpp"
+# include <iostream>
 
 # define ERRCHAN_CREATION 	"channel creation error : "
 
@@ -26,7 +28,8 @@ class ChannelManager {
 
 	t_mapChannel			_chanList;
 
-	void	log(std::string const &logMsg)	const;
+	bool	isChannelExists(std::string const &channelName)	const;
+	void	log(std::string const &logMsg)					const;
 
  public :
 	ChannelManager();
@@ -37,7 +40,7 @@ class ChannelManager {
 	void	tryAddNewChannel(std::string const &name, Client &chop);
 	void	rmChannel(std::string const &name);
 
-
+	void	addClientToChannel(Client &user, std::string const &channelName);
 	// [+] addclient to a channel
 	// remove client from a channel (check si channel est vide apres)
 
